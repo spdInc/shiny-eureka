d99aefa6852e74cf7ad5b5afab0bda73850b19ec
harsh3311 <harshbhardwajofficial1414@gmail.com>
2022-10-10 14:04:56 +0530

    Created Operators.java
    Please merge this in main. would help me in hacktoberfest 2022.
    May it help you.

---
+++ Operators.java
@@ -0,0 +1,20 @@
+public class Operators {
+    public static void main(String arg[]) {
+        System.out.println("Hello World");
+        int a = 1;
+        int b = 4;
+        int sum = a + b;
+        System.out.println(sum);
+        int diff = a - b;
+        System.out.println(diff);
+        int mul = a * b;
+        System.out.println(mul);
+        int div = a / b;
+        System.out.println(div);
+        int abc = 2 * (a + b) + (a - b);
+        System.out.println(abc);
+        int defg = 2 * (a * b);
+        System.out.println(defg);
+        System.out.println("*\n**\n***\n****");
+    }
+}
